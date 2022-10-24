71870048d4972159d29629d68c40327e4e05f9c3
Nexus5614 <115270774+Nexus5614@users.noreply.github.com>
2022-10-24 13:26:19 +0500

    3

---
+++ 3.R
@@ -0,0 +1,24 @@
+msg <- readLines('putin-2018.txt')
+
+#посчитаем кол-во символов
+length(msg)
+l <- lapply(msg, nchar)
+Reduce(sum,l)
+
+#можно не заморачиваться
+txt <- paste(msg, collapse = " ")
+nchar(txt)
+
+r <- grep('мы',msg)
+r
+msg[r]
+
+r <- grep('Мы', msg)
+r
+msg[r]
+
+words <- unlist(strsplit(txt, ' '))
+wc <- table(words)
+wc <- sort(wc, decreasing = TRUE)
+head(wc,20)
+
